59de21c71a7863983f4c51d1b7b90ea710e91ed5
Chathura_Janithya <it21263880@my.sliit.lk>
2023-10-05 02:55:35 +0530

    chathura | editted generalEventDetails
    login(fonts),Signup(font,margin)

--- frontend/screens/generalEventDetails.jsx
+++ frontend/screens/generalEventDetails.jsx
@@ -15,6 +15,7 @@ const { width, height } = Dimensions.get("window");
 
 const GeneralEventDetails = ({ route }) => {
   const { item } = route.params;
+  console.log(item);
 
   const navigation = useNavigation();
   const handleBack = () => {
@@ -23,25 +24,58 @@ const GeneralEventDetails = ({ route }) => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView>
-        <View style={styles.container}>
-          <View style={styles.imageContainer}>
-            <Image
-              source={require("../assets/images/sampleEvent.jpeg")}
-              style={styles.eventImg}
-              resizeMode="cover"
-            />
-            <View style={styles.darkLayer} />
-            <TouchableOpacity style={styles.backButton} onPress={handleBack}>
-              <Image source={require("../assets/images/backIcon.png")} />
-            </TouchableOpacity>
-            <Text style={styles.eventTittle}>Viramaya</Text>
-            <TouchableOpacity style={styles.buyTicketBtn}>
-              <Text style={styles.buyTicketText}>Buy Tickets</Text>
-            </TouchableOpacity>
-          </View>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/images/sampleEvent.jpeg")}
+            style={styles.eventImg}
+            resizeMode="cover"
+          />
+          <View style={styles.darkLayer} />
+          <TouchableOpacity style={styles.backButton} onPress={handleBack}>
+            <Image source={require("../assets/images/backIcon.png")} />
+          </TouchableOpacity>
+          <Text style={styles.eventTitle}>Viramaya</Text>
+          <Text style={styles.eventDesc} numberOfLines={3}>
+            The SLIIT was established in 1999 to educate and train IT ...
+            Wiramaya – විරාමය 2022 organized by Faculty of Computing Student
+            Community (FCSC) of SLIIT will be held on 26th of February 2022 at
+            SLIIT Malabe Campus.
+          </Text>
         </View>
-      </ScrollView>
+        <View style={styles.photos}>
+          <TouchableOpacity>
+            <Text style={styles.photosText}>Photos</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.eventDetails}>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Date: </Text>
+            26th February 2022
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Time: </Text>
+            8.00 am
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Venue: </Text>
+            SLIIT Malabe Campus
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Organized by: </Text>
+            FCSC
+          </Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>Contact: </Text>
+            0771234567
+          </Text>
+        </View>
+        <View style={styles.buyTicketSec}>
+          <TouchableOpacity style={styles.buyTicketBtn}>
+            <Text style={styles.buyTicketText}>Buy Tickets</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -51,6 +85,7 @@ export default GeneralEventDetails;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
   },
   imageContainer: {
     position: "relative",
@@ -65,29 +100,75 @@ const styles = StyleSheet.create({
   },
   backButton: {
     position: "absolute",
-    top: "5%",
-    left: "0.2%",
+    top: height * 0.02,
+    left: width * 0.02,
   },
-  eventTittle: {
+  eventTitle: {
     position: "absolute",
-    bottom: "37%",
+    bottom: height * 0.167,
     fontSize: width * 0.12,
     color: "#fff",
     fontWeight: "bold",
     textTransform: "uppercase",
     textShadowColor: "rgba(0, 0, 0, 0.5)",
-    alignSelf: "center",
+    left: width * 0.03,
+  },
+  eventDesc: {
+    position: "absolute",
+    bottom: height * 0.085,
+    fontSize: width * 0.035,
+    color: "grey",
+    textShadowColor: "rgba(0, 0, 0, 0.5)",
+    left: width * 0.04,
+    width: "70%",
+  },
+  photos: {
+    position: "absolute",
+    width: "100%",
+    top: height * 0.42,
+    borderTopRightRadius: width * 0.1,
+    borderTopLeftRadius: width * 0.1,
+    backgroundColor: "#16213E",
+  },
+  photosText: {
+    fontSize: width * 0.055,
+    padding: width * 0.05,
+    color: "#fff",
+    top: height * 0,
+    left: width * 0.01,
+    paddingBottom: height * 0.06,
+  },
+  eventDetails: {
+    position: "absolute",
+    backgroundColor: "#fff",
+    width: "100%",
+    top: height * 0.5,
+    borderTopLeftRadius: width * 0.1,
+    borderTopRightRadius: width * 0.1,
+    padding: width * 0.05,
+    gap: height * 0.038,
+  },
+  buyTicketSec: {
+    position: "absolute",
+    width: "100%",
+    top: height * 0.84,
+    alignItems: "center",
+    backgroundColor: "rgb(260,120,76)",
+    borderTopLeftRadius: width * 0.1,
+    borderTopRightRadius: width * 0.1,
+    bottom: 0,
   },
   buyTicketBtn: {
-    position: "absolute",
-    bottom: "8%",
-    backgroundColor: "#fff",
-    borderRadius: width * 0.01,
-    padding: width * 0.01,
-    right: "5%",
+    padding: width * 0.03,
+    borderRadius: width * 0.1,
+    alignItems: "center",
+    width: "45%",
+    alignSelf: "center",
+    marginTop: height * 0.01,
   },
   buyTicketText: {
-    fontSize: width * 0.03,
+    color: "#fff",
+    fontSize: width * 0.05,
     fontWeight: "bold",
   },
 });
