76e9a8132a303750f79418db3612549c55174d5f
Chathura_Janithya <it21263880@my.sliit.lk>
2023-10-24 21:07:46 +0530

    chathura | updated EventAnalytics all sponsors

--- frontend/components/eventanalytics.jsx
+++ frontend/components/eventanalytics.jsx
@@ -1,37 +1,135 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { PieChart, BarChart } from "react-native-chart-kit";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 const EventAnalytics = ({ pastevent }) => {
+  const [event, setEvent] = useState(pastevent);
+  const [budget, setBudget] = useState(pastevent.totalBudget);
+  const [ticket, setTicket] = useState(Number(pastevent.soldTickets) || 0);
+  const [allTickets, setAllTickets] = useState(
+    Number(pastevent.alltickets) || 0
+  );
+  const [crowd, setCrowd] = useState(Number(pastevent.participatedCrowd));
+  const [expectedCrowd, setExpectedCrowd] = useState(
+    Number(pastevent.expectedCrowd)
+  );
+  const [expectedBudget, setExpectedBudget] = useState(
+    Number(pastevent.expectedBudget)
+  );
+
+  console.log(pastevent.soldTickets, "sold tickets");
+  console.log(pastevent.alltickets, "sold tickets");
+
+  console.log(pastevent);
+  const pieChartConfig = {
+    backgroundGradientFrom: "#f9f9f9",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#f9f9f9",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2,
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false,
+  };
+
+  const barChartConfig = {
+    backgroundGradientFrom: "#f9f9f9",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#f9f9f9",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 3) => `rgba(0, 0, 0, ${opacity})`,
+  };
+
+  const pieChartData = [
+    {
+      name: "Sold Tickets",
+      ticket: ticket,
+      color: "darkred",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 12,
+    },
+    {
+      name: "Available Tickets",
+      ticket: allTickets,
+      color: "green",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 12,
+    },
+  ];
+
+  const budgetData = {
+    labels: ["Budget", "Ex.Budget"],
+    datasets: [
+      {
+        data: [100000, 80000],
+      },
+    ],
+  };
+  const crowdData = {
+    labels: ["Expected Crowd", "Participated Crowd"],
+    datasets: [
+      {
+        data: [100, 80],
+      },
+    ],
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.analyticsHeader}>Event Analytics</Text>
-      {renderAnalyticsItem('Sold Tickets:', pastevent.soldTickets, 'darkred')}
-      {renderAnalyticsItem('Available Tickets:', pastevent.alltickets, 'green')}
-      {renderAnalyticsItem('Expected Crowd:', pastevent.expectedCrowd, 'royalblue')}
-      {renderAnalyticsItem('Expected Budget:', `$${pastevent.expectedBudget}`, 'royalblue')}
-      {renderAnalyticsItem('Total Budget:', `$${pastevent.totalBudget}`, 'royalblue')}
-      {renderAnalyticsItem('Participated Crowd:', pastevent.participatedCrowd, 'darkgreen')}
+
+      <Text style={{ fontSize: width * 0.05, fontWeight: "bold" }}>
+        Ticket Data
+      </Text>
+      <View style={styles.ticketData}>
+        <PieChart
+          data={pieChartData}
+          width={width * 0.9}
+          height={height * 0.3}
+          chartConfig={pieChartConfig}
+          accessor={"ticket"}
+          backgroundColor={"transparent"}
+          absolute
+        />
+      </View>
+
+      <View style={styles.barCharts}>
+        <Text style={{ fontSize: width * 0.05, fontWeight: "bold" }}>
+          Ticket Data
+        </Text>
+        <BarChart
+          data={budgetData}
+          width={width * 0.9}
+          height={height * 0.3}
+          chartConfig={barChartConfig}
+          fromZero={true}
+          showValuesOnTopOfBars={true}
+        />
+        <Text style={{ fontSize: width * 0.05, fontWeight: "bold" }}>
+          Crowd
+        </Text>
+        <BarChart
+          data={crowdData}
+          width={width * 0.9}
+          height={height * 0.3}
+          chartConfig={barChartConfig}
+          fromZero={true}
+          showValuesOnTopOfBars={true}
+        />
+      </View>
     </View>
   );
 };
 
-const renderAnalyticsItem = (label, value, color, description) => (
-  <View style={{ ...styles.analyticsItem, borderColor: color }}>
-    <Text style={{ ...styles.analyticsLabel, color: color }}>{label}</Text>
-    <Text style={{ ...styles.analyticsValue, color: color }}>{value}</Text>
-    <Text style={styles.analyticsDescription}>{description}</Text>
-  </View>
-);
-
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f9f9f9',
+    backgroundColor: "#f9f9f9",
     padding: 16,
     borderRadius: 10,
     margin: 8,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
@@ -40,35 +138,25 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   analyticsHeader: {
-    fontFamily: 'DMMedium',
-    fontSize: 26,
+    fontFamily: "DMMedium",
+    fontSize: width * 0.08,
     letterSpacing: 3,
-    color: 'black',
-    fontWeight: 'bold',
+    color: "black",
+    fontWeight: "bold",
     marginBottom: 16,
+    textAlign: "center",
   },
-  analyticsItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginVertical: 10,
-    borderWidth: 2,
-    borderRadius: 5,
-    padding: 8,
-  },
-  analyticsLabel: {
+  ticketData: {
     flex: 1,
-    fontSize: 16,
-    fontFamily: 'DMRegular',
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: height * 0.05,
   },
-  analyticsValue: {
-    fontSize: 16,
-    fontFamily: 'DMMedium',
-  },
-  analyticsDescription: {
-    fontSize: 14,
-    color: 'gray',
-    marginTop: 4,
+  barCharts: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
   },
 });
 
